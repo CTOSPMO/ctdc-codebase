7b89e01f67fa74b70f7e448c9dfb11d45d6c1717
Ajay Doddapaneni <doddapaneni.ajay@gmail.com>
2020-01-28 15:41:22 -0500

    yml content

--- src/main/frontend/src/pages/yml-at-buildtime/yml-at-buildtime.js
+++ src/main/frontend/src/pages/yml-at-buildtime/yml-at-buildtime.js
@@ -1,12 +1,34 @@
-import React from 'react';
-import YAMLData from './My-YAML-Content.yaml';
+import React, { useState, useEffect } from 'react';
+import yaml from 'js-yaml';
+import axios from 'axios';
+import YAMLData1 from './My-YAML-Content.yaml';
+import l9dg from '../../assets/about/About_Purpose.png';
+import Body from './BodyView';
+import Header from '../../components/About/HeaderView';
+import Stats from '../../components/Stats/AllStatsController';
 
-const YAMLbuildtime = () => (
-  <div style={{ maxWidth: '960px', margin: '1.45rem' }}>
-    <h1>{YAMLData.title}</h1>
-    <ul>
-      {YAMLData.content.map((data, index) => <li key={`content_item_${index}`}>{data.item}</li>)}
-    </ul>
-  </div>
-);
+
+const YAMLbuildtime = () => {
+  const [data1, setData] = useState([]);
+
+
+  useEffect(async () => {
+    const result = await axios(YAMLData1);
+    const yamlData2 = yaml.safeLoad(result.data);
+    const supportObj = yamlData2.find(({ page }) => page === '/purpose');
+    setData(supportObj);
+  }, []);
+
+  return (
+    <>
+      <Stats />
+      <Header title={data1.title} />
+      <Body data={{
+        img: l9dg,
+        content: data1.content,
+      }}
+      />
+    </>
+  );
+};
 export default YAMLbuildtime;
